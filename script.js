c95f6570ca137f1f9bc9cb3d321d45c1efe6a755
Victor W <victorwbarros@gmail.com>
2022-04-08 14:30:36 -0300

    Layout Completo/ Embaralhamento Funcionando(gambiarra)/ Falta fazer o Click e o counterClick

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
+const listaCartas = [];
+const cartas = document.querySelector(".linha");
+const cartasCostas= ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif", "metalparrot.gif","metalparrot.gif","revertitparrot.gif","revertitparrot.gif","tripletsparrot.gif","tripletsparrot.gif","unicornparrot.gif","unicornparrot.gif"]
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
 function inserirCartas(){
-const quantCartas = Number(prompt("Com quantas cartas você que jogar? (insira um número par entre 4-14)"));
+let quantCartas = Number(prompt("Com quantas cartas você que jogar? (insira um número par entre 4-14)"));
 const verificarPar= quantCartas % 2;
-const max =(quantCartas / 2) - 1;
+const max =quantCartas  - 1;
 
  if(quantCartas>= 4 && quantCartas<= 14 && verificarPar === 0 ){
-      const listaCartas = []
-      const cartas = document.querySelectorAll(".h");
       for(let i=0; i<=max; i++){
-         let card =`<div class="carta" onclick="counterClick()" >
-      <div class="frente face">
+         let card =`<div class="carta " onclick="virar(this)"  >
+      <div class=" frente face" >
         <img src="imagens/front.png" alt="">
       </div>
-      <div class="costas face">
-        costas${i}
+      <div class=" costas face">
+      <img src="imagens/${cartasCostas[i]}" alt="">
       </div>
         </div>`;
-        console.log(listaCartas)
         listaCartas.push(card)
       }
-      listaCartas.sort(comparador);
-      for(let i=0; i<=cartas.length-1; i++){
-        
-        cartas[i].innerHTML =listaCartas.join('');
-      }
+      embaralhar();
     }else{
           alert("Por favor, insira um número par entre 4 e 14"); 
           inserirCartas();
         }
 }
-function embaralhar(){
-  let cartas = document.querySelectorAll(".h");
-  console.log(cartas)
-}
-embaralhar();
-
 inserirCartas()
+
+function embaralhar(){
+ listaCartas.sort(comparador);
+      cartas.innerHTML =listaCartas.join('');
+}
+
+function virar(elemento){
+  
+  console.log(elemento)
+}
+
 let contador =0;
 function counterClick(){
   contador++;
 }
+
 function displayCounter(){
   
 }
