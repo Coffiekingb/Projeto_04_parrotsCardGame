654cd3ca118dd052d785c67d17ff849d65b8edc7
Victor W <victorwbarros@gmail.com>
2022-04-11 09:47:36 -0300

    Projeto final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let arrayParrots =[];
+let cartasViradas=[];
 let contador =0;
 const listaCartas = [];
 const cartas = document.querySelector(".linha");
@@ -11,12 +13,12 @@ const verificarPar= quantCartas % 2;
 
  if(quantCartas>= 4 && quantCartas<= 14 && verificarPar === 0 ){
       for(let i=0; i<quantCartas; i++){
-         let card =`<div class="carta "  onclick="verificarCarta()">
+         let card =`<div class="carta " >
+          <div class=" face" onclick="chama3(this)"  >
           <div class=" costas face">
            <img src="imagens/${cartasCostas[i]}" alt="">
           </div>
-          <div class=" frente face" onclick="chama3(this)"  >
-           <img src="imagens/front.png" >
+           <div class= "frente face"><img src="imagens/front.png" > </div>
           </div>
          </div>`;
         listaCartas.push({card})  
@@ -50,25 +52,46 @@ function inserirCarta(){
 }
 
 function virar(elemento){
- elemento.classList.add("sumir");
+ elemento.lastElementChild.classList.add("sumir");
+ verificarCarta(elemento);
+}
 
+function desvirar(cartasViradas){
+let ultimaCartaVirada=cartasViradas[cartasViradas.length-1];
+ let penultimaCartaVirada=cartasViradas[cartasViradas.length-2];
+  console.log(ultimaCartaVirada)
+  ultimaCartaVirada.classList.remove("sumir");
+  penultimaCartaVirada.classList.remove("sumir");
+}
+function verificarCarta(elemento){
+  arrayParrots.push(elemento.firstElementChild.innerHTML)
+  cartasViradas.push(elemento.lastElementChild)
+ const verificarPar= cartasViradas.length % 2;
+ let ultimaCartaVirada=arrayParrots[arrayParrots.length-1];
+ let penultimaCartaVirada=arrayParrots[arrayParrots.length-2];
+ console.log(cartasViradas)
  
- setTimeout(desvirar, 1000, elemento)
-  console.log(elemento)
-  
+ if(verificarPar!== 1 && cartasViradas.length>=2){
+     if(ultimaCartaVirada!==penultimaCartaVirada){
+     setTimeout(desvirar, 1000, cartasViradas)
+     setTimeout(timeoutClick, 1000, cartasViradas)
+    }else{
+      setTimeout(displayCounter, 100)
+    }
+ }  
 }
-function desvirar(bla){
-  console.log("rodei")
-  bla.classList.remove("sumir");
-}
-function verificarCarta(){
-  console.log("rodei tbm")
 
+function timeoutClick(carta){
+ carta.document.querySelector("onclick").remove("onclick='chama3(this)'")
+ setTimeout(toBackClick, 1000, cartas)
+}
+function toBackClick(carta){
+  carta.document.querySelector("onclick").add("onclick='chama3(this)'")
 }
 function chama3(botao){
   virar(botao);
+  counterClick();
   
-
 }
 
 function counterClick(){
@@ -77,9 +100,14 @@ function counterClick(){
 }
 
 function displayCounter(){
+  
+ let fim = document.querySelectorAll(".sumir")
+ console.log("fim")
 
+ if(fim.length===listaCartas.length){
   alert(`Parabéns! Você ganhou em ${contador} jogadas!`)
 }
+  }
 function comparador() { 
 	return Math.random() - 0.5; 
 }
